01d7b3a484769ff50775a677318e699858557877
KagurazakaNyaa <i@kagurazakanyaa.com>
2022-08-11 17:25:53 +0800

    增加基本功能

--- lib/src/main/java/com/nyaabot/common/BaseMessageQueueManager.java
+++ lib/src/main/java/com/nyaabot/common/BaseMessageQueueManager.java
@@ -4,10 +4,13 @@ import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.api.trace.Tracer;
 
+import com.nyaabot.common.model.Message;
 import com.nyaabot.common.utils.ConfigUtils;
 import com.nyaabot.common.utils.OpenTelemetryUtils;
+import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -40,5 +43,84 @@ public abstract class BaseMessageQueueManager {
             span.end();
         }
     }
+
+    /**
+     * 声明队列
+     * 
+     * @param queueName 队列名称
+     * @throws IOException
+     * @throws TimeoutException
+     */
+    public void declareQueue(String queueName) throws IOException, TimeoutException {
+        Span span = tracer.spanBuilder("Start to declare queue " + queueName).startSpan();
+        try (Channel channel = connection.createChannel()) {
+            channel.queueDeclare(queueName, true, false, false, null);
+        } catch (IOException | TimeoutException e) {
+            span.setStatus(StatusCode.ERROR);
+            span.recordException(e);
+            throw e;
+        } finally {
+            span.end();
+        }
+    }
+
+    /**
+     * 声明交换机
+     * 
+     * @param exchangeName 交换机名称
+     * @throws IOException
+     * @throws TimeoutException
+     */
+    public void declareExchange(String exchangeName) throws IOException, TimeoutException {
+        Span span = tracer.spanBuilder("Start to declare exchange " + exchangeName).startSpan();
+        try (Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(exchangeName, "topic", true);
+        } catch (IOException | TimeoutException e) {
+            span.setStatus(StatusCode.ERROR);
+            span.recordException(e);
+            throw e;
+        } finally {
+            span.end();
+        }
+    }
+
+    /**
+     * 绑定队列到交换机
+     * 
+     * @param queueName    队列名称
+     * @param exchangeName 交换机名称
+     * @param routingKey   路由键
+     * @throws IOException
+     * @throws TimeoutException
+     */
+    public void bindQueue(String queueName, String exchangeName, String routingKey)
+            throws IOException, TimeoutException {
+        Span span = tracer.spanBuilder("Start to bind queue " + queueName + " to exchange " + exchangeName).startSpan();
+        try (Channel channel = connection.createChannel()) {
+            channel.queueBind(queueName, exchangeName, routingKey);
+        } catch (IOException | TimeoutException e) {
+            span.setStatus(StatusCode.ERROR);
+            span.recordException(e);
+            throw e;
+        } finally {
+            span.end();
+        }
+    }
+
+    /**
+     * 发送消息
+     * 
+     * @param exchangeName 交换机名称
+     * @param message      消息
+     */
+    public abstract void sendMessage(String exchangeName, Message message);
+
+    /**
+     * 绑定消息接收处理器
+     * 
+     * @param queueName 队列名称
+     * @param callback  消息接收回调函数
+     */
+    public abstract void bindCallback(String queueName, DeliverCallback callback);
 
 }
